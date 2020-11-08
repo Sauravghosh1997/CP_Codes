52047451acee671f4b32c3be7c12cf0e674a0369
Saurav Ghosh <sauravansh17@gmail.com>
2020-11-08 19:55:58 +0530

    Add files via upload

--- CSS/main.cpp
+++ CSS/main.cpp
@@ -1,1 +1,36 @@
+//CSS: https://www.codechef.com/problems/CSS2/
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    unordered_map< int,unordered_map<int,pair<int,int>> > mp;
+    while(n--)
+    {
+        int id,attr,val,pri;
+        cin>>id>>attr>>val>>pri;
+        if(mp.find(id)== mp.end())
+            mp[id][attr]= make_pair(val, pri);
+        else
+        {
+            if(mp[id].find(attr) == mp[id].end())
+                mp[id][attr]= make_pair(val,pri);
+            else
+            {
+                if(pri >= mp[id][attr].second)
+                    mp[id][attr]=make_pair(val,pri);
+            }
+        }
+    }
+    while(m--)
+    {
+        int id, attr;
+        cin>>id>>attr;
+        cout<<mp[id][attr].first<<endl;
+    }
+    return 0;
+}
